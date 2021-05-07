2c1682a8daca2eded9b963c9f9dbd996e19fcd9a
BetoMonster <betomonster@gmail.com>
2021-05-06 23:25:33 -0500

    first server NODEjs

---
+++ index.js
@@ -0,0 +1,17 @@
+const http =require('http')
+
+const server = http.createServer((request, response)=>{
+    console.log('url: ',request.url)
+    console.log('method: ',request.method)
+    response.write('Hola desde un servidor NODE')
+    response.end()
+})
+
+server.listen(8080,() => {
+    console.log('servidor escuchando en el puerto 8080')
+})
+
+//llamada a
+
+//https:localhost:8080
+
