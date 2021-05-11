e2c1f016ae76ef886ab998873e48c85465e05711
BetoMonster <betomonster@gmail.com>
2021-05-10 20:05:42 -0500

    request koder first server

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const http =require('http')
 const server = http.createServer((request, response)=>{
     console.log('url: ',request.url)
     console.log('method: ',request.method)
-    response.write('Hola desde un servidor NODE')
+    
+    //response.write('Hola desde un servidor NODE')
+
+    if (request.url==='/koder'&& request.method==='GET'){
+        response.write('aqui estan todos los koders')
+    }else if (request.url==='/koder'&& request.method==='POST'){
+        response.write('aqui puedes crear koders')
+    }else{
+        response.write('No estaba preparado para eso :c')
+    }
+
     response.end()
 })
 
@@ -15,3 +25,6 @@ server.listen(8080,() => {
 
 //https:localhost:8080
 
+//GET /koder -> aqui estan todos los koders
+//POST / koder -> aqui pudes crear koders
+//X/X -> No estaba preparado para eso :c
